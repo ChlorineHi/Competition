f92154fbeb88f43f23238349555b1f1cc3ed3ed9
ChlorineHi <2532807089@qq.com>
2024-12-13 19:27:36 +0800

    Upload app.py and App.vue

--- tryTwo.py
+++ tryTwo.py
@@ -1,8 +1,16 @@
 from langchain_community.chat_models import ChatZhipuAI
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 import os
+from dotenv import load_dotenv
 
-os.environ["ZHIPUAI_API_KEY"] = "5add66299015d43308c9f484d4cf622b.PrBnxyI02FvdcJiO"
+# 加载环境变量
+load_dotenv()
+
+# 从环境变量中获取 API 密钥
+api_key = os.getenv("ZHIPUAI_API_KEY")
+if api_key is None:
+    raise ValueError("API key not found. Please set the ZHIPUAI_API_KEY environment variable.")
+
 chat = ChatZhipuAI(
     model="glm-4",
     temperature=0.5,
